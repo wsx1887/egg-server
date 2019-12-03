e85302f041e9e30ea66f543fba493e1f20afcb3d
王仕献 <wangshixian@suipianny.com>
2019-12-04 00:04:42 +0800

    aixos测试

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const Axios = require('axios');
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
+  }
+  async request() {
+    const { ctx } = this;
+    const res = await Axios.get('', { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' });
+    // console.log(res);
+    ctx.set({ 'Content-Type': 'text/html; charset=gbk' });
+    ctx.body = res.data;
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -6,5 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/request', controller.home.request);
   router.get('/api/wechatmini/index', controller.api.weChatMini.home.index);
 };
